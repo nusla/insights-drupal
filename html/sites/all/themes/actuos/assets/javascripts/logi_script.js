36e7eea608d237cb94596bf335b26f42659bc89b
Vardan Bagiryan <vardan.bagiryan@gmail.com>
2015-02-19 19:31:54 -0800

    added daterangepicker styling

--- html/sites/all/themes/actuos/assets/javascripts/logi_script.js
+++ html/sites/all/themes/actuos/assets/javascripts/logi_script.js
@@ -7,15 +7,14 @@ $(function() {
 		
 		if ($('#filter-dropdown-content').is(':visible')){
 			$('#filter-dropdown-content').hide();
-			$('#filter-direction-icon').removeClass('fa-angle-down');
-			$('#filter-direction-icon').addClass('fa-angle-up');
-			$(this).addClass('clear-bottom-border');
+			$('#filter-direction-icon').addClass('fa-angle-down');
+			$('#filter-direction-icon').removeClass('fa-angle-up');
+			$(this).removeClass('clear-bottom-border');
 		} else {
 			$('#filter-dropdown-content').show();
 			$('#filter-direction-icon').addClass('fa-angle-down');
 			$('#filter-direction-icon').removeClass('fa-angle-up');
-			$(this).removeClass('clear-bottom-border');
+			$(this).addClass('clear-bottom-border');
 		}
 	});
-	
 });
