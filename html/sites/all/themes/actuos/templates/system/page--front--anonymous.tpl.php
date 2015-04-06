97443933837bfed3840afe7fba6f42d8e8411887
Vardan Bagiryan <vardan.bagiryan@gmail.com>
2015-04-06 11:09:03 -0700

    removed animation

--- html/sites/all/themes/actuos/templates/system/page--front--anonymous.tpl.php
+++ html/sites/all/themes/actuos/templates/system/page--front--anonymous.tpl.php
@@ -4,13 +4,13 @@
 			<div class="col-md-3 col-sm-3"></div>
 			<div class="col-md-6 col-sm-6 logoholder hidden-xs">
 				<img src="<?php print base_path() . drupal_get_path('theme', 'actuos'); ?>/assets/images/main_logo.png"
-					class="actuos_logo animated bounceInDown">
+					class="actuos_logo bounceInDown">
 			</div>
 			<div class="col-md-3 col-sm-3"></div>
 		</div>
 		<div class="row">
 			<div class="col-md-3 col-sm-3"></div>
-			<div class="col-md-6 col-sm-6 loginform_holder animated bounceInRight">
+			<div class="col-md-6 col-sm-6 loginform_holder bounceInRight">
 				<div class="login-form-wrapper">
   					<?php print $login_content; ?>
 				</div>
