6cfc8edfcfe2f90d83b14bb9db34c88030329f82
bigsauron <vardan.bagiryan@gmail.com>
2015-10-18 18:00:37 -0700

    add row

--- html/sites/all/themes/insights/templates/system/page.tpl.php
+++ html/sites/all/themes/insights/templates/system/page.tpl.php
@@ -3,16 +3,20 @@
 
 <div class="row">
 	<div class="container">
+	  <div class="row">
 		<div class="col-md-8 protfolio-title">PORTFOLIO INSIGHTS TOOL</div>
 		<div class="col-md-4 protfolio-logo"><img src="<?php print $logo; ?>" /></div>
+  	  </div>
 	</div>
 </div>
 <div class="row main-menu-container">
 	<div class="container">
+	  <div class="row">
 		<div class="col-md-12">
 			<?php print render($main_menu); ?>
 			<?php print render($user_menu); ?>
 			<div class="menu-pipe"></div>
+		  </div>
 		</div>
 	</div>
 </div>
